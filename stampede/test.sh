9cc2482ff75a09cfcd3df7c40771157a0846c61e
Matthew Miller <mattmill@login4.stampede2.tacc.utexas.edu>
2018-08-14 17:37:19 -0500

    updated stampede test

--- stampede/test.sh
+++ stampede/test.sh
@@ -5,16 +5,27 @@
 #SBATCH -n 16
 #SBATCH -t 00:30:00
 #SBATCH -p development
-#SBATCH -J test-mu18SV4
+#SBATCH -J test-16S
 #SBATCH --mail-type BEGIN,END,FAIL
-#SBATCH --mail-user jklynch@email.arizona.edu
+#SBATCH --mail-user mattmiller899@email.arizona.edu
 
-OUT_DIR="$SCRATCH/muscope-18SV4/test"
+module load tacc-singularity
 
-if [[ -d $OUT_DIR ]]; then
-  rm -rf $OUT_DIR
-fi
+singularity run $WORK/16S_pipeline/imicrobe-16SrDNA-OTU-Clustering.img \
+    --input-dir $WORK/16S_pipeline/data_in/ \
+    --pear-min-overlap 10 \
+    --pear-max-assembly-length 500 \
+    --pear-min-assembly-length 220 \
+    --vsearch-filter-maxee 1 \
+    --vsearch-filter-trunclen 245 \
+    --vsearch-derep-minuniquesize 4 \
+    --core-count 4 \
+    --multiple-runs \
+    --cutadapt-min-length 100
 
-mkdir -p $OUT_DIR
+#arg_parser.add_argument('--forward-primer', default='ATTAGAWACCCVNGTAGTCC',
+#                            help='forward primer to be clipped by cutadapt')
 
-./run.sh "$SCRATCH/muscope-18SV4/test-data/" $OUT_DIR
+#    arg_parser.add_argument('--reverse-primer', default='TTACCGCGGCKGCTGGCAC',
+#                            help='reverse primer to be clipped by cutadapt')
+#${INPUT_DIR} ${OUTPUT_DIR} ${core_count} ${cutadapt_min_length} ${pear_min_overlap} ${pear_max_assembly_length} ${pear_min_assembly_length} ${vsearch_filter_maxee} ${vsearch_filter_trunclen} ${vsearch_derep_minuniquesize} ${forward_primer} ${reverse_primer} ${multiple_runs} ${run_help}
