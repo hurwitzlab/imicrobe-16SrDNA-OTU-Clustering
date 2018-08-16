f1fd263845b99e0108cfb6e11e2a37f68451cc8e
Matthew Miller <mattmill@login1.stampede2.tacc.utexas.edu>
2018-08-16 12:48:25 -0500

    changes to stampede stuff, app works now

--- stampede/run.sh
+++ stampede/run.sh
@@ -5,8 +5,4 @@ module load tacc-singularity
 set -u
 IMG="/work/05286/mattmill/stampede2/16S_pipeline/imicrobe-16SrDNA-OTU-Clustering.img"
 singularity run "$IMG" "$@" 
-#sleep 10
-#export LAUNCHER_PPN=2
-
-#$LAUNCHER_DIR/paramrun
-echo "Ended launcher"
+echo "Done."
--- stampede/template.sh
+++ stampede/template.sh
@@ -2,8 +2,21 @@
 
 echo "Started $(date)"
 
-echo "INPUT_DIR: ${INPUT_DIR} \nOUTPUT_DIR: ${OUTPUT_DIR} \n UCHIME_REF_DB: ${UCHIME_REF_DB} \ncore_count: ${core_count}\n cutadapt_min_length: ${cutadapt_min_length}\npear_min_overlap: ${pear_min_overlap}\npear_max_assembly_length: ${pear_max_assembly_length}\npear_min_assembly_length: ${pear_min_assembly_length}\nvsearch_filter_maxee: ${vsearch_filter_maxee}\nvsearch_filter_trunclen: ${vsearch_filter_trunclen}\n vsearch_derep_minuniquesize: ${vsearch_derep_minuniquesize}\nforward_primer: ${forward_primer}\nreverse_primer: ${reverse_primer}\n multiple_runs: ${multiple_runs}\n"
+echo "INPUT_DIR: ${INPUT_DIR}"
+echo "OUTPUT_DIR: ${OUTPUT_DIR}"
+echo "UCHIME_REF_DB: ${UCHIME_REF_DB}"
+echo "core_count: ${core_count}"
+echo "cutadapt_min_length: ${cutadapt_min_length}"
+echo "pear_min_overlap: ${pear_min_overlap}"
+echo "pear_max_assembly_length: ${pear_max_assembly_length}"
+echo "pear_min_assembly_length: ${pear_min_assembly_length}"
+echo "vsearch_filter_maxee: ${vsearch_filter_maxee}"
+echo "vsearch_filter_trunclen: ${vsearch_filter_trunclen}"
+echo "vsearch_derep_minuniquesize: ${vsearch_derep_minuniquesize}"
+echo "forward_primer: ${forward_primer}"
+echo "reverse_primer: ${reverse_primer}"
+echo "multiple_runs: ${multiple_runs}"
+
 sh run.sh ${INPUT_DIR} ${OUTPUT_DIR} ${UCHIME_REF_DB} ${core_count} ${cutadapt_min_length} ${pear_min_overlap} ${pear_max_assembly_length} ${pear_min_assembly_length} ${vsearch_filter_maxee} ${vsearch_filter_trunclen} ${vsearch_derep_minuniquesize} ${forward_primer} ${reverse_primer} ${multiple_runs}
 
 echo "Ended $(date)"
-exit 0
