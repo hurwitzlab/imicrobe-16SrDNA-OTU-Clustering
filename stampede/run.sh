67c82dc1822d5b2a8b041cd7b8c264091b4228d1
Matthew Miller <mattmill@login2.stampede2.tacc.utexas.edu>
2018-08-20 16:02:16 -0500

    changed path to img

--- stampede/run.sh
+++ stampede/run.sh
@@ -3,6 +3,6 @@
 module load tacc-singularity
 
 set -u
-IMG="/work/05066/imicrobe/singularity/imicrobe-16SrDNA-OTU-Clustering.img"
+IMG="/work/05066/imicrobe/singularity/imicrobe-16SrDNA-OTU-Clustering-0.0.1.img"
 singularity run "$IMG" "$@" 
 echo "Done."
