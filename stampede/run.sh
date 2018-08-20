1a0bdcb205182d7395c45002e6ce85bdac59a0b6
Matthew Miller <mattmill@login2.stampede2.tacc.utexas.edu>
2018-08-20 16:00:15 -0500

    finished app

--- stampede/run.sh
+++ stampede/run.sh
@@ -3,6 +3,6 @@
 module load tacc-singularity
 
 set -u
-IMG="/work/05286/mattmill/stampede2/16S_pipeline/imicrobe-16SrDNA-OTU-Clustering.img"
+IMG="/work/05066/imicrobe/singularity/imicrobe-16SrDNA-OTU-Clustering.img"
 singularity run "$IMG" "$@" 
 echo "Done."
