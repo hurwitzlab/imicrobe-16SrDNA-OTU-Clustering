b1cb6d91b1a195ce97f4ab39d4794ea6cf5e77e3
Matthew Miller <mattmill@login4.stampede2.tacc.utexas.edu>
2018-08-29 15:43:39 -0500

    final changes to app stuff

--- stampede/template.sh
+++ stampede/template.sh
@@ -16,7 +16,10 @@ echo "vsearch_derep_minuniquesize: ${vsearch_derep_minuniquesize}"
 echo "forward_primer: ${forward_primer}"
 echo "reverse_primer: ${reverse_primer}"
 echo "multiple_runs: ${multiple_runs}"
+echo "paired_ends: ${paired_ends}"
+echo "debug: ${debug}"
+echo "steps: ${steps}"
 
-sh run.sh ${INPUT_DIR} ${OUTPUT_DIR} ${UCHIME_REF_DB} ${core_count} ${cutadapt_min_length} ${pear_min_overlap} ${pear_max_assembly_length} ${pear_min_assembly_length} ${vsearch_filter_maxee} ${vsearch_filter_trunclen} ${vsearch_derep_minuniquesize} ${forward_primer} ${reverse_primer} ${multiple_runs}
+sh run.sh ${INPUT_DIR} ${OUTPUT_DIR} ${UCHIME_REF_DB} ${core_count} ${cutadapt_min_length} ${pear_min_overlap} ${pear_max_assembly_length} ${pear_min_assembly_length} ${vsearch_filter_maxee} ${vsearch_filter_trunclen} ${vsearch_derep_minuniquesize} ${forward_primer} ${reverse_primer} ${multiple_runs} ${paired_ends} ${steps} ${debug}
 
 echo "Ended $(date)"
