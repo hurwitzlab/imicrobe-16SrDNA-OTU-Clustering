fd5e8911a68a80d475f2e8a5269af3c1c53f7335
Matthew Miller <mattmill@login4.stampede2.tacc.utexas.edu>
2018-08-29 16:22:38 -0500

    smaall change to template

--- stampede/template.sh
+++ stampede/template.sh
@@ -3,7 +3,6 @@
 echo "Started $(date)"
 
 echo "INPUT_DIR: ${INPUT_DIR}"
-echo "OUTPUT_DIR: ${OUTPUT_DIR}"
 echo "UCHIME_REF_DB: ${UCHIME_REF_DB}"
 echo "core_count: ${core_count}"
 echo "cutadapt_min_length: ${cutadapt_min_length}"
@@ -20,6 +19,6 @@ echo "paired_ends: ${paired_ends}"
 echo "debug: ${debug}"
 echo "steps: ${steps}"
 
-sh run.sh ${INPUT_DIR} ${OUTPUT_DIR} ${UCHIME_REF_DB} ${core_count} ${cutadapt_min_length} ${pear_min_overlap} ${pear_max_assembly_length} ${pear_min_assembly_length} ${vsearch_filter_maxee} ${vsearch_filter_trunclen} ${vsearch_derep_minuniquesize} ${forward_primer} ${reverse_primer} ${multiple_runs} ${paired_ends} ${steps} ${debug}
+sh run.sh ${INPUT_DIR} ${UCHIME_REF_DB} ${core_count} ${cutadapt_min_length} ${pear_min_overlap} ${pear_max_assembly_length} ${pear_min_assembly_length} ${vsearch_filter_maxee} ${vsearch_filter_trunclen} ${vsearch_derep_minuniquesize} ${forward_primer} ${reverse_primer} ${multiple_runs} ${paired_ends} ${steps} ${debug}
 
 echo "Ended $(date)"
