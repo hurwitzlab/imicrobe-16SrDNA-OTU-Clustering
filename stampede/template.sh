96d3a52fa7daa6c0a3f7043ec9dad8aaa41d45e5
Matthew Miller <mattmill@login2.stampede2.tacc.utexas.edu>
2018-09-12 16:06:08 -0500

    updated stampede stuff

--- stampede/template.sh
+++ stampede/template.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 echo "Started $(date)"
-
+echo "Inputs"
 echo "INPUT_DIR: ${INPUT_DIR}"
 echo "UCHIME_REF_DB: ${UCHIME_REF_DB}"
+echo "CUTADAPT_3PRIME_ADAPTER_FILE_FORWARD: ${CUTADAPT_3PRIME_ADAPTER_FILE_FORWARD}"
+echo "CUTADAPT_3PRIME_ADAPTER_FILE_REVERSE: ${CUTADAPT_3PRIME_ADAPTER_FILE_REVERSE}"
+echo "CUTADAPT_5PRIME_ADAPTER_FILE_FORWARD: ${CUTADAPT_5PRIME_ADAPTER_FILE_FORWARD}"
+echo "CUTADAPT_5PRIME_ADAPTER_FILE_REVERSE: ${CUTADAPT_5PRIME_ADAPTER_FILE_REVERSE}"
+echo ""
+echo "Parameters"
 echo "core_count: ${core_count}"
 echo "cutadapt_min_length: ${cutadapt_min_length}"
 echo "pear_min_overlap: ${pear_min_overlap}"
@@ -12,13 +18,15 @@ echo "pear_min_assembly_length: ${pear_min_assembly_length}"
 echo "vsearch_filter_maxee: ${vsearch_filter_maxee}"
 echo "vsearch_filter_trunclen: ${vsearch_filter_trunclen}"
 echo "vsearch_derep_minuniquesize: ${vsearch_derep_minuniquesize}"
-echo "forward_primer: ${forward_primer}"
-echo "reverse_primer: ${reverse_primer}"
+echo "forward_primer_3prime: ${forward_primer_3prime}"
+echo "reverse_primer_3prime: ${reverse_primer_3prime}"
+echo "forward_primer_5prime: ${forward_primer_5prime}"
+echo "reverse_primer_5prime: ${reverse_primer_5prime}"
 echo "multiple_runs: ${multiple_runs}"
 echo "paired_ends: ${paired_ends}"
 echo "debug: ${debug}"
 echo "steps: ${steps}"
 
-sh run.sh ${INPUT_DIR} ${UCHIME_REF_DB} ${core_count} ${cutadapt_min_length} ${pear_min_overlap} ${pear_max_assembly_length} ${pear_min_assembly_length} ${vsearch_filter_maxee} ${vsearch_filter_trunclen} ${vsearch_derep_minuniquesize} ${forward_primer} ${reverse_primer} ${multiple_runs} ${paired_ends} ${steps} ${debug}
+sh run.sh ${INPUT_DIR} ${UCHIME_REF_DB} ${CUTADAPT_3PRIME_ADAPTER_FILE_FORWARD} ${CUTADAPT_3PRIME_ADAPTER_FILE_REVERSE} ${CUTADAPT_5PRIME_ADAPTER_FILE_FORWARD} ${CUTADAPT_5PRIME_ADAPTER_FILE_REVERSE} ${core_count} ${cutadapt_min_length} ${pear_min_overlap} ${pear_max_assembly_length} ${pear_min_assembly_length} ${vsearch_filter_maxee} ${vsearch_filter_trunclen} ${vsearch_derep_minuniquesize} ${forward_primer_3prime} ${reverse_primer_3prime} ${forward_primer_5prime} ${reverse_primer_5prime} ${multiple_runs} ${paired_ends} ${steps} ${debug}
 
 echo "Ended $(date)"
