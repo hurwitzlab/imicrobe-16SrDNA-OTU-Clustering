f0485d1e4e49b97726d2c964a18677ad3890312f
Matthew Miller <mattmill@login2.stampede2.tacc.utexas.edu>
2018-09-12 20:46:56 -0500

    changed run to point to stored sing image

--- stampede/run.sh
+++ stampede/run.sh
@@ -5,6 +5,6 @@ module load tacc-singularity
 set -u
 
 #IMG="/work/05066/imicrobe/singularity/imicrobe-16SrDNA-OTU-Clustering-0.0.1.img"
-IMG="/work/05286/mattmill/stampede2/imicrobe-16SrDNA-OTU-Clustering-0.0.1.img"
+IMG="/work/05066/imicrobe/singularity/imicrobe-16SrDNA-OTU-Clustering-0.0.1.img"
 singularity run "$IMG" "$@" 
 echo "Done."
